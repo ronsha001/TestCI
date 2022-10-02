7a5ea1623dab2bc6238444ffa6e78848b3799991
Ron Sharabi <ronsh0111@gmail.com>
2022-10-02 10:54:45 +0300

    webhook.py

---
+++ webhook.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, Response
+
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def health():
+  if request.method == 'POST':
+    print (request.data)
+    return Response("This was a POST request", status=200)
+  elif request.method == 'GET':
+    return Response("This was a GET request", status=200)
+
+  return Response("Error", status=500)
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port='8081')
